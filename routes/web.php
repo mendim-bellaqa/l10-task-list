d38115798611251d5983c28058e56a667ce97573
bellaqamendim@gmail.com <bellaqamendim@gmail.com>
2023-12-22 03:04:31 +0100

    Save data to the db

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-use \App\Models\Task;
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -33,5 +33,17 @@ Route::get('/tasks/{id}', function ($id) {
 })->name('tasks.show');
 
 Route::match(['get', 'post'], '/tasks/store', function (Request $request) {
-  dd($request->all());
+  $data = $request->validate([
+      'title' => 'required|max:255',
+      'description' => 'required',
+      'long_description' => 'required',
+  ]);
+
+  $task = new Task;
+  $task->title = $data['title'];
+  $task->description = $data['description'];
+  $task->long_description = $data['long_description'];
+  $task->save();
+
+  return redirect()->route('tasks.show', ['id' => $task->id]);
 })->name('tasks.store');
