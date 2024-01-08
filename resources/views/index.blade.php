3ebe45d6eeed47d8116667aeacb705326b3776e0
bellaqamendim@gmail.com <bellaqamendim@gmail.com>
2024-01-08 01:45:47 +0100

    V1-Backend-Frontend

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,12 +3,16 @@
 @section('title', 'The list of tasks')
 @section('content')
 
+
+
 <nav class="mb-4">
     <a href="{{ route('tasks.create') }}" class="font-medium text-gray-700 underline decoration-pink-500">Add Task!</a>
+    
+  
 </nav>
 
 @forelse ($tasks as $task)
-    <div class="text-center">
+    <div class="text-left">
         <a href="{{ route('tasks.show', ['task' => $task->id]) }}" class="{{ $task->completed ? 'line-through' : '' }}">{{ $task->title }}</a>
     </div>
 @empty
@@ -23,5 +27,13 @@
     </div>
 @endif
 
+<script>
+    // Dark mode toggle logic
+    const darkModeToggle = document.getElementById('darkModeToggle');
+
+    darkModeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+</script>
+
 @endsection
-F@
